5a26e20e45b6f7fb3197e924d433bc8e5ecff124
Luftpump <krautheimer.j@gmail.com>
2025-11-12 16:27:22 +0100

    Added format check to CDataPerturb.data_perturbation

--- src/data_perturb/CDataPerturb.py
+++ src/data_perturb/CDataPerturb.py
@@ -6,6 +6,8 @@ class CDataPerturb(ABC):
 
     @abstractmethod
     def data_perturbation(self, x):
+        if x.shape[1] != 1:
+            raise ValueError("Input data must be a 2D array with a single feature column.")
         pass
 
     def perturb_dataset(self, X : np.ndarray):
